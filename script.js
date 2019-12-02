2abc9f11dac6ab0bc30ec5963e3c7bacf9a53274
Trudy Jones <tjones374@gmail.com>
2019-12-01 22:56:33 -0700

    Saving code commit

--- script.js
+++ script.js
@@ -4,10 +4,20 @@ var queryURL =
 	'https://samples.openweathermap.org/data/2.5/weather?' +
 	'q=London&units=imperial&appid=' +
 	APIKey;
-
+//console.log keeps stating a type error: $.ajax is not a function. Double triple checked and synatax is correct. Am I missing a script somewhere on the html?
 $.ajax({
 	url: queryURL,
 	method: 'GET'
 });
 
-console.log(response);
+//Need to add to add classes to html
+$('.city').html('<h1>' + response.name + ' Weather Details</h1>');
+$('.wind').text('Wind Speed: ' + response.wind.speed);
+$('.humidity').text('Humidity: ' + response.main.humidity);
+$('.temp').text('Temperature (F) ' + response.main.temp);
+
+console.log('Wind Speed: ' + response.wind.speed);
+
+// Questions
+//where do I put this click event? Both in html and js?
+// $(".fa").click(function () {
