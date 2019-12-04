03c2bd710ac8a4505bb5547173813af57fa03fbb
Trudy Jones <tjones374@gmail.com>
2019-12-03 21:27:05 -0700

    Updated js

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
 $(document).ready(function() {});
-var APIKey = 'f33edbda95e6349a1022bdc669ee8886';
-var queryURL =
-	'https://samples.openweathermap.org/data/2.5/weather?' +
-	'q=London&units=imperial&appid=' +
-	APIKey;
-//console.log keeps stating a type error: $.ajax is not a function. Double triple checked and synatax is correct. Am I missing a script somewhere on the html?
-$.ajax({
-	url: queryURL,
-	method: 'GET'
-});
+function forecastFive(city) {
+	var APIKey = 'f33edbda95e6349a1022bdc669ee8886';
+	var queryURL =
+		'https://api.openweathermap.org/data/2.5/forecast?q=' +
+		city +
+		',us&units=imperial' +
+		'&appid=' +
+		APIKey;
 
-//Need to add to add classes to html
-$('.city').html('<h1>' + response.name + ' Weather Details</h1>');
-$('.wind').text('Wind Speed: ' + response.wind.speed);
-$('.humidity').text('Humidity: ' + response.main.humidity);
-$('.temp').text('Temperature (F) ' + response.main.temp);
+	$.ajax({
+		url: queryURL,
+		method: 'GET',
+		dataType: 'json'
+	}).then(function(data) {
+		for (i = 0; i < data.list.length; i++) {
+			var checker = data.list[i].dt_text.indexOf('12:00:00');
+			if (checker === 11) {
+				console.log(data.list[i].dt_txt);
+			}
+		}
+		console.log(data);
+	});
 
-console.log('Wind Speed: ' + response.wind.speed);
+	//Need to add to add classes to html
+	// $('.city').html('<h1>' + response.name + ' Weather Details</h1>');
+	// $('.wind').text('Wind Speed: ' + response.wind.speed);
+	// $('.humidity').text('Humidity: ' + response.main.humidity);
+	// $('.temp').text('Temperature (F) ' + response.main.temp);
 
+	// console.log(response);
+}
 // Questions
 //where do I put this click event? Both in html and js?
 // $(".fa").click(function () {
+forecastFive('Baltimore');
